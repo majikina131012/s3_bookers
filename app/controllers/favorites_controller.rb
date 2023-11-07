b98c2de87563ffb5996c1e428726f2491cc5bc37
majikina131012 <majikina.ryuma.infratop@gmail.com>
2023-11-07 11:26:42 +0000

    課題８ｃメンバーのグループ加入機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -6,6 +6,16 @@ class FavoritesController < ApplicationController
     favorite.save
     redirect_to request.referer
   end
+  
+  # def create
+  # 	book = Book.find(params[:book_id])
+  # 	favorite = Favorite.new
+  # 	favorite.user_id = current_user.id
+  # 	favorite.book_id = book.id
+  # 	favorite.save
+  # 	redirect_to request.referer
+  # end 
+  # 上の記述と同じ！
 
   def destroy
     @book = Book.find(params[:book_id])
--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,7 @@ Rails.application.routes.draw do
       resource :relationships, only: [:create, :destroy]
     end  
     
-  resources :groups, only: [:new, :index, :show, :create, :edit, :update]  
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end
 end
