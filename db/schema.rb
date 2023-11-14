1be35bd864085d2542dce64380d2d9a05ea3b01a
majikina131012 <majikina.ryuma.infratop@gmail.com>
2023-11-14 12:14:55 +0000

    星評価機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_27_050900) do
+ActiveRecord::Schema.define(version: 2023_11_13_113203) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_10_27_050900) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+    t.string "star"
   end
 
   create_table "favorites", force: :cascade do |t|
